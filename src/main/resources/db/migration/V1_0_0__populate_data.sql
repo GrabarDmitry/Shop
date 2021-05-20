df0373e902950ace640c1e23f276dd1975d5ff53
GrabarDmitry <grabar.dimulya@mail.ru>
2021-05-20 21:16:15 +0300

    Add migration

--- src/main/resources/db/migration/V1_0_0__populate_data.sql
+++ src/main/resources/db/migration/V1_0_0__populate_data.sql
@@ -2,10 +2,13 @@ use portfolio;
 
 SET foreign_key_checks = 0;
 
-DELETE FROM portfolio.User WHERE id <> 0;
-ALTER TABLE portfolio.User AUTO_INCREMENT = 1;
+DELETE
+FROM portfolio.User
+WHERE id <> 0;
+ALTER TABLE portfolio.User
+    AUTO_INCREMENT = 1;
 
 INSERT INTO User(dateOfBirth, email, name, password, surname)
-VALUES ('08.06.1999', 'dima@mail.ru','Dzmitry','12345','Hrabar'),
-       ('29.08.2003', 'sasha@mail.ru','Sasha','54321','Hrabar'),
-       ('27.03.2007','olya@mail.ru','Olha','123','Hrabar')
+VALUES ('1999-06-08', 'dima@mail.ru', 'Dzmitry', '12345', 'Hrabar'),
+       ('2003-08-29', 'sasha@mail.ru', 'Sasha', '54321', 'Hrabar'),
+       ('2007-03-27', 'olya@mail.ru', 'Olha', '123', 'Hrabar')
