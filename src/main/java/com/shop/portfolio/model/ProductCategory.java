876eaf400c5810994f0c73e991cc1bf199158ae5
GrabarDmitry <grabar.dimulya@mail.ru>
2021-05-21 00:04:28 +0300

    add models

---
+++ src/main/java/com/shop/portfolio/model/ProductCategory.java
@@ -0,0 +1,21 @@
+package com.shop.portfolio.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductCategory extends AbstractEntity {
+
+    @Column(length = 45)
+    private String title;
+
+}
---
+++ src/main/java/com/shop/portfolio/model/ProductImage.java
@@ -0,0 +1,25 @@
+package com.shop.portfolio.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductImage extends AbstractEntity {
+
+    private String path;
+
+    @ManyToOne
+    @JoinColumn(name = "productId")
+    private Product product;
+
+}
